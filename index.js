2e01ea6d53c9d038010b2a43d026b49821d7cf89
Delight <user.delightvincent487@gmail.com>
2024-03-23 07:34:53 +0000

    commit message

---
+++ index.js
@@ -0,0 +1,28 @@
+
+let counter = document.getElementById("count-el")
+
+ let count = 0
+
+function increament() {
+      count = count + 1
+         counter.innerText = count
+ 
+}
+
+function decrease() {
+      count = count - 1
+       counter.innerText = count
+       
+      
+}
+
+
+function save() {
+     zero = 0
+     counter.innerText = zero
+}
+
+
+
+
+
